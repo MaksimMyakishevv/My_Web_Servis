0d762009f5a13672fbc73671472f2a9e985eb12e
MaksimMyakishevv <130791600+MaksimMyakishevv@users.noreply.github.com>
2024-02-01 13:28:56 +0300

    create file .env and connect him with main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const port: number = parseInt(process.env.PORT);
+  const server = process.env.SERVER;
+  console.log('port for conection', port, server);
+  await app.listen(port, server);
+  console.log(`aplication is runnig on: ${await app.getUrl()}`);
 }
 bootstrap();
